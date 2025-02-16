7d4b038e56414c5021f4c9431143cbf3a76a267b
Lê Tuấn Anh <anhlt24032003@gmail.com>
2025-02-16 15:44:53 +0700

    Fix decode jwt to get UserId

--- VinWallet.API/Service/BaseService.cs
+++ VinWallet.API/Service/BaseService.cs
@@ -41,7 +41,7 @@ namespace VinWallet.API.Service
 
         protected string GetUserIdFromJwt()
         {
-            string userId = _httpContextAccessor?.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userId = _httpContextAccessor?.HttpContext?.User.FindFirstValue("UserId");
             return userId;
         }
     }
