3de2eb6ff5f60a2af435444ff9c5703ef3063ba8
khoadase172677 <khoadase172677@fpt.edu.vn>
2025-03-01 22:40:31 +0700

    hotfix

--- VinWallet.API/Service/Implements/UserService.cs
+++ VinWallet.API/Service/Implements/UserService.cs
@@ -33,45 +33,74 @@ namespace VinWallet.API.Service.Implements
 
         public async Task<UserResponse> CreateUser(CreateUserRequest createUserRequest)
         {
-            var existUser = await _unitOfWork.GetRepository<User>().SingleOrDefaultAsync(predicate: x => x.Username.Equals(createUserRequest.Username));
-            if (existUser != null) throw new BadHttpRequestException(MessageConstant.UserMessage.UsernameAlreadyExists);
+            // Kiểm tra Username đã tồn tại chưa
+            var existUser = await _unitOfWork.GetRepository<User>()
+                .SingleOrDefaultAsync(predicate: x => x.Username.Equals(createUserRequest.Username));
 
+            if (existUser != null)
+                throw new BadHttpRequestException(MessageConstant.UserMessage.UsernameAlreadyExists);
+
+            // Kiểm tra Email đã tồn tại chưa
+            var existEmail = await _unitOfWork.GetRepository<User>()
+                .SingleOrDefaultAsync(predicate: x => x.Email.Equals(createUserRequest.Email));
+
+            if (existEmail != null)
+                throw new BadHttpRequestException(MessageConstant.UserMessage.EmailAlreadyExists);
+
+            // Kiểm tra PhoneNumber đã tồn tại chưa
+            var existPhoneNumber = await _unitOfWork.GetRepository<User>()
+                .SingleOrDefaultAsync(predicate: x => x.PhoneNumber.Equals(createUserRequest.PhoneNumber));
+
+            if (existPhoneNumber != null)
+                throw new BadHttpRequestException(MessageConstant.UserMessage.PhoneNumberAlreadyExists);
+
+            // Kiểm tra Building và House
             var urlBuilding = HomeCleanApiEndPointConstant.Building.BuildingByCodeEndpoint.Replace("{code}", createUserRequest.BuildingCode);
             var urlHouse = HomeCleanApiEndPointConstant.House.HouseByCodeEndpoint.Replace("{code}", createUserRequest.HouseCode);
 
             var buildingResponse = await CallApiUtils.CallApiEndpoint(urlBuilding, null);
             var houseResponse = await CallApiUtils.CallApiEndpoint(urlHouse, null);
 
-            var buiding = await CallApiUtils.GenerateObjectFromResponse<BuildingResponse>(buildingResponse);
-            if (buiding == null) throw new BadHttpRequestException(MessageConstant.BuildingMessage.BuildingNotFound);
+            var building = await CallApiUtils.GenerateObjectFromResponse<BuildingResponse>(buildingResponse);
+            if (building == null)
+                throw new BadHttpRequestException(MessageConstant.BuildingMessage.BuildingNotFound);
 
             var house = await CallApiUtils.GenerateObjectFromResponse<HouseResponse>(houseResponse);
-            if (house == null) throw new BadHttpRequestException(MessageConstant.HouseMessage.HouseNotFound);
+            if (house == null)
+                throw new BadHttpRequestException(MessageConstant.HouseMessage.HouseNotFound);
 
-            if (!house.BuildingId.Equals(buiding.Id)) throw new BadHttpRequestException(MessageConstant.HouseMessage.HouseNotInBuilding);
+            if (!house.BuildingId.Equals(building.Id))
+                throw new BadHttpRequestException(MessageConstant.HouseMessage.HouseNotInBuilding);
 
-
+            // Tạo User mới
             var newUser = _mapper.Map<User>(createUserRequest);
             newUser.Id = Guid.NewGuid();
             newUser.Password = PasswordUtil.HashPassword(createUserRequest.Password);
             newUser.Status = UserEnum.Status.Active.ToString();
             newUser.HouseId = house.Id;
 
+            // Gán Role mặc định
+            Role role = await _unitOfWork.GetRepository<Role>()
+                .SingleOrDefaultAsync(predicate: x => x.Name.Equals(UserEnum.Role.Member.ToString()));
 
-            Role role = await _unitOfWork.GetRepository<Role>().SingleOrDefaultAsync(predicate: x => x.Name.Equals(UserEnum.Role.Member.ToString()));
             newUser.RoleId = role.Id;
             newUser.CreatedAt = DateTime.UtcNow.AddHours(7);
             newUser.UpdatedAt = DateTime.UtcNow.AddHours(7);
 
+            // Lưu User vào DB
             await _unitOfWork.GetRepository<User>().InsertAsync(newUser);
-            if (await _unitOfWork.CommitAsync() <= 0) throw new DbUpdateException(MessageConstant.DataBase.DatabaseError);
+            if (await _unitOfWork.CommitAsync() <= 0)
+                throw new DbUpdateException(MessageConstant.DataBase.DatabaseError);
 
+            // Tạo ví cá nhân cho User
             _backgroundJobClient.Enqueue(() => _walletService.CreatePersionalWallet(newUser.Id));
 
+            // Trả về Response
             var response = _mapper.Map<UserResponse>(newUser);
             response.Role = role.Name;
             return response;
         }
+
 
 
 
