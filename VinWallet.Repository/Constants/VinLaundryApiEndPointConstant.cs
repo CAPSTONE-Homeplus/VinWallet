9d384a91a4267a76a507d26e9054697a1ec255da
khoadase172677 <khoadase172677@fpt.edu.vn>
2025-04-05 19:21:03 +0700

    done-transaction

---
+++ VinWallet.Repository/Constants/VinLaundryApiEndPointConstant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinWallet.Repository.Constants
+{
+    public static class VinLaundryApiEndPointConstant
+    {
+        static VinLaundryApiEndPointConstant()
+        {
+        }
+        public const string baseUrl = "https://localhost:7019";
+        public const string RootEndPoint = baseUrl + "/api";
+        public const string ApiVersion = "/v1";
+        public const string ApiEndpoint = RootEndPoint + ApiVersion;
+
+        public static class Order
+        {
+            public const string OrdersEndpoint = ApiEndpoint + "/orders";
+            public const string OrderEndpoint = OrdersEndpoint + "/{id}";
+            public const string OrderEndpointForPayment = OrderEndpoint + "/payment";
+        }
+    }
+}
---
+++ VinWallet.Repository/Payload/Response/OrderResponse/OrderResponseForPaymentLaundry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinWallet.Repository.Payload.Response.OrderResponse
+{
+    public class OrderResponseForPaymentLaundry
+    {
+        public Guid Id { get; set; }
+
+        public string? OrderCode { get; set; }
+
+        public string? Name { get; set; }
+
+        public Guid? UserId { get; set; }
+
+        public decimal? Balance { get; set; }
+
+        public string? Currency { get; set; }
+
+        public string? Type { get; set; }
+
+        public string? ExtraField { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+
+        public decimal? DiscountAmount { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public DateTime? DeliveryDate { get; set; }
+
+        public string? Status { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? EstimatedCompletionTime { get; set; }
+
+        public Guid? AppliedDiscountId { get; set; }
+    }
+}
